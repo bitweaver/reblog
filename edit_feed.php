cbf18b0c48eae16b97aa1c97523206e2c5646fc6
wjames5 <will@tekimaki.com>
2009-10-01 14:17:03 +0000

    remove ref to non-existant copyright.txt file

--- edit_feed.php
+++ edit_feed.php
@@ -1,7 +1,7 @@
 <?php
-/** $Header: /cvsroot/bitweaver/_bit_reblog/edit_feed.php,v 1.7 2009/10/01 13:45:46 wjames5 Exp $
+/** $Header: /cvsroot/bitweaver/_bit_reblog/edit_feed.php,v 1.8 2009/10/01 14:17:03 wjames5 Exp $
  * Copyright (c) 2002-2003, Luis Argerich, Garland Foster, Eduardo Polidor, et. al.
- * All Rights Reserved. See copyright.txt for details and a complete list of authors.
+ * All Rights Reserved. See below for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details.
  * @package reblog
  * @subpackage functions
